2cc71469dad36473551e1650494c45ed60c181aa
fsdFelipe <98924712+fsdFelipe@users.noreply.github.com>
2022-11-29 11:55:21 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import { BrowserRouter, Route, Routes } from "react-dom";
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
